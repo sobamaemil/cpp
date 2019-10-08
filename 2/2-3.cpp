e336b56f893df9f2db4cc9350ec9d20857763e56
sobamaemil <56301498+sobamaemil@users.noreply.github.com>
2019-10-08 20:22:21 +0900

    Create 2-3.cpp

---
+++ 2/2-3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	cout << "두 수를 입력하라>>";
+	cin >> a >> b;
+	cout << "큰 수 = ";
+	if(a<b) cout << b;
+	else cout << a;
+	
+	return 0; 
+}
