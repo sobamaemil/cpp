ac11be94a8724620bba7bf20e4bccf56b9e333c5
sobamaemil <56301498+sobamaemil@users.noreply.github.com>
2019-10-08 20:20:49 +0900

    Create 0-3.cpp

---
+++ 0/0-3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,i,j,g;
+	cout << "정수 입력>>";
+	cin >> a;
+	
+/*	for(i=0; i<a; i++){
+		j = a-i;
+		for(j>i;j--;) cout << "*";
+		cout << "\n";
+		
+	} 
+*/
+
+
+/*	i = 0;
+	j = 0;
+	while(i<a){
+		j = a - i;
+		while(j>0){
+			cout << "*";
+			j--;
+		}
+		cout << endl;
+		i++;
+	}
+*/
+
+
+/*	i = 0;
+	j = 0;
+	a-=1;
+	
+	do{
+		j = a - i;
+		do{
+			cout << "*";
+			j--;
+		}while(j>=0);
+		cout << endl;
+		i++;
+	}while(i<=a);
+*/
+
+	return 0;
+}
